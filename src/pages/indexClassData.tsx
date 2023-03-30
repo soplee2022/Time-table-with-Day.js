5a914b1dd167c0ee6b07499ad87383a8fee62856
Sophie <2020soop@gmail.com>
2023-03-30 15:42:33 +0800

    [Feat] change weekData, add NewtWeek/PreviousWeek btn

--- src/pages/indexClassData.tsx
+++ src/pages/indexClassData.tsx
@@ -1,19 +1,42 @@
-import React, { useState } from "react";
-import { calssData } from "./data";
+import React, { useEffect, useState } from "react";
+import { weekData } from "./data";
 import Link from "next/link";
 
 export default function Home() {
+  // 計算週數
+  const [weekNum, setWeekNum] = useState(0);
+  const [nextWeek, setNextWeek] = useState(weekData[0].weekDataAry);
+  useEffect(() => {
+    setNextWeek(weekData[weekNum].weekDataAry);
+  }, [weekNum]);
+
+  // 點擊按鈕時，取得下週所有時段
+  const getNextWeek = () => {
+    setWeekNum((prev) => {
+      return prev + 1;
+    });
+  };
+
+  // 點擊按鈕時，取得上週所有時段
+  const getPreviousWeek = () => {
+    if (weekNum > 0) {
+      setWeekNum((prev) => {
+        return prev - 1;
+      });
+    }
+  };
 
   return (
     <>
       <main className="bg-[#FFFCF6]">
-        {/* {console.log(classData[0])} */}
-        <h1 className=" text-4xl py-3 px-[19px] space-y-1 bg-[#FFFCF6]">模擬接 API</h1>
+        <h1 className=" text-4xl py-3 px-[19px] space-y-1 bg-[#FFFCF6]">
+          模擬接 API
+        </h1>
 
         <div className="space-y-4">
           <ul className="flex text-center space-x-3  h-[376px] overflow-auto border hour-scrollbar">
-            {calssData.map((item) => (
-              <li key={item.date}>
+            {nextWeek.map((item, i) => (
+              <li key={i}>
                 <div className="border-b-2 border-b-[#424242] w-[56px] py-3 px-[19px] space-y-1 fixed bg-[#FFFCF6]">
                   <p className="text-lg">{item.weekDay}</p>
                   <p className="text-base">{item.date}</p>
@@ -38,15 +61,26 @@ export default function Home() {
             ))}
           </ul>
           <div className="flex justify-between w-[464px] ">
-            <button className="py-3 text-sm text-[#424242] w-[220px] rounded-[10px] border-[1.5px] border-[#424242] font-semibold">
+            <button
+              className="py-3 text-sm text-[#424242] w-[220px] rounded-[10px] border-[1.5px] border-[#424242] font-semibold"
+              onClick={getPreviousWeek}
+            >
               上一週
             </button>
-            <button className="py-3 text-sm text-[#424242] w-[220px] rounded-[10px] border-[1.5px] border-[#424242] font-semibold">
+            <button
+              className="py-3 text-sm text-[#424242] w-[220px] rounded-[10px] border-[1.5px] border-[#424242] font-semibold"
+              onClick={getNextWeek}
+            >
               下一週
             </button>
           </div>
           <div className="mt-10">
-            <Link href="./" className="py-3 text-sm text-[#424242] px-[60px] rounded-[10px] bg-[#FFEFCD] font-semibold">前端 Day.js</Link>
+            <Link
+              href="./"
+              className="py-3 text-sm text-[#424242] px-[60px] rounded-[10px] bg-[#FFEFCD] font-semibold"
+            >
+              前端 Day.js
+            </Link>
           </div>
         </div>
       </main>
